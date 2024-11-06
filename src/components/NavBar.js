f8a2dc093c9757987bf499ee567e239bce1ce26c
Timea Mali <derzsi_timea@yahoo.com>
2024-11-06 12:42:53 +0000

    add style for navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,32 +1,33 @@
-import React from 'react'
+import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from "../styles/NavBar.module.css";
 
 
 const NavBar = () => {
-  return ( 
-  <Navbar expand="md" fixed="top">
-    <Container>
-        <Navbar.Brand >
-            <img src={logo} alt="logo" height="70" width="75" />
-             </Navbar.Brand>
-             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-             <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="ml-auto text-left">
-                    <Nav.Link>
-                <i class="fa-solid fa-magnifying-glass"></i>Discover
-                    </Nav.Link>
-                <Nav.Link>
-                    <i className="fas fa-sign-in-alt"></i>Sign in
-                </Nav.Link>
-                <Nav.Link>
-                <i className="fas fa-user-plus"></i>Sign up
-                </Nav.Link>
-                </Nav>
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="75" width="85" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link >
+            <i class="fa-solid fa-magnifying-glass"></i>Discover
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
-    </Container>
-  </Navbar>
+      </Container>
+    </Navbar>
   );
 };
 
-export default NavBar
+export default NavBar;
