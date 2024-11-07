933b4da3123a94531f500279d496d45eb2966730
Timea Mali <derzsi_timea@yahoo.com>
2024-11-07 12:06:30 +0000

    configure access and refresh token handling for secure user authentication

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://api-retrospective-3d1e13d99a31.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
