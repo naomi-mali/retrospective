9c46b63472808fe302dee97d2f558fff86e566d4
Timea Mali <derzsi_timea@yahoo.com>
2024-11-10 11:06:39 +0000

    create PopularProfiles Component

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -17,6 +17,8 @@ import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
 
+import PopularProfiles from "../profiles/PopularProfiles";
+
 
 
 function PostsPage({ message, filter = "" }) {
@@ -50,7 +52,7 @@ function PostsPage({ message, filter = "" }) {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles mobile</p>
+      <PopularProfiles mobile />
         <i className={`fas fa-search ${styles.SearchIcon}`} />
         <Form
           className={styles.SearchBar}
@@ -90,7 +92,7 @@ function PostsPage({ message, filter = "" }) {
         )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-        <p>Who to follow</p>
+      <PopularProfiles/>
       </Col>
     </Row>
   );
---
+++ src/pages/profiles/PopularProfiles.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+
+const PopularProfiles = ({ mobile }) => {
+  const [profileData, setProfileData] = useState({
+    // we will use the pageProfile later!
+    pageProfile: { results: [] },
+    popularProfiles: { results: [] },
+  });
+  const { popularProfiles } = profileData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData((prevState) => ({
+          ...prevState,
+          popularProfiles: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
+  return (
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+    }`}
+    style={{borderRadius: "15px"}}
+    >
+      {popularProfiles.results.length ? (
+        <>
+          <p>Who to follow.</p>
+          {mobile ? (
+            <div className="d-flex justify-content-between flex-wrap overflow-hidden">
+              {popularProfiles.results.slice(0, 10).map((profile) => (
+                <p key={profile.id} className="mx-2">{profile.owner}</p>
+              ))}
+            </div>
+          ) : (
+            popularProfiles.results.map((profile) => (
+              <p key={profile.id}>{profile.owner}</p>
+            ))
+          )}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
+    </Container>
+  );
+};
+
+export default PopularProfiles;
