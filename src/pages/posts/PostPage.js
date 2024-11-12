98cc523c27cb870fab72f25e7e3cd05b6b6c77ad
Timea Mali <derzsi_timea@yahoo.com>
2024-11-12 00:57:24 +0000

    update post page

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -16,11 +16,11 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Asset from "../../components/Asset";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function PostPage() {
   const { id } = useParams();
   const [post, setPost] = useState({ results: [] });
-
 
   const currentUser = useCurrentUser();
   const profile_image = currentUser?.profile_image;
@@ -36,7 +36,7 @@ function PostPage() {
         setPost({ results: [post] });
         setComments(comments);
       } catch (err) {
-        console.log(err);
+        // console.log(err);
       }
     };
 
@@ -46,48 +46,47 @@ function PostPage() {
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
-        <p>Popular profiles for mobile</p>
+        <PopularProfiles mobile />
         <Post {...post.results[0]} setPosts={setPost} postPage />
-        <Container className={appStyles.Content}
-        style={{ borderRadius: '30px', width: '700px', }}>
-{currentUser ? (
+        <Container className={appStyles.Content}>
+          {currentUser ? (
             <CommentCreateForm
-            profile_id={currentUser.profile_id}
-            profileImage={profile_image}
-            post={id}
-            setPost={setPost}
-            setComments={setComments}
-          />
-        ) : comments.results.length ? (
-          "Comments"
-        ) : null}
-        {comments.results.length ? (
-          <InfiniteScroll
-            children={comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setPost={setPost}
-                setComments={setComments}
-              />
-            ))}
-            dataLength={comments.results.length}
-            loader={<Asset spinner />}
-            hasMore={!!comments.next}
-            next={() => fetchMoreData(comments, setComments)}
-          />
-        ) : currentUser ? (
-          <span>No comments yet, be the first to comment!</span>
-        ) : (
-          <span>No comments... yet</span>
-        )}
-      </Container>
-    </Col>
-    <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-      Popular profiles for desktop
-    </Col>
-  </Row>
-);
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
+          {comments.results.length ? (
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
+          ) : currentUser ? (
+            <span>No comments yet, be the first to comment!</span>
+          ) : (
+            <span>No comments... yet</span>
+          )}
+        </Container>
+      </Col>
+      <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
+        <PopularProfiles />
+      </Col>
+    </Row>
+  );
 }
 
 export default PostPage;
